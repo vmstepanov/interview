6f187bf763539b7cede0a16b67dc3a11a509c193
vmstepanov <vmstepanov@mail.ru>
2021-06-19 00:12:15 +0300

    Method refs question

--- src/main/java/i/lang/lambda/LambdaTask1.java
+++ src/main/java/i/lang/lambda/LambdaTask1.java
@@ -1,6 +1,10 @@
 package i.lang.lambda;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.IntFunction;
 import java.util.function.Supplier;
 
 public class LambdaTask1 {
@@ -8,13 +12,16 @@ public class LambdaTask1 {
     public static void main(String[] args) {
 
         // запишите через ссылку на метод
-        Supplier<List<String>> supplier = null;
+        Supplier<List<String>> supplier = ArrayList<String>::new;
 
         // запишите через ссылку на метод копирующий конструктор
-
         {
-            // Запишите через java.util.function левую часть выражения
-            // = String[]::new;
+            Function<Collection<String>, List<String>> f = ArrayList<String>::new;
+        }
+
+        // Запишите через java.util.function левую часть выражения
+        {
+            IntFunction<String[]> x = String[]::new;
         }
 
     }
